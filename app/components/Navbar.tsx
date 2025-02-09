c0587276553186faec26bfea459f5d8bb8d3dd70
daksh3003 <dakshpandey3003@gmail.com>
2025-02-09 15:36:37 +0530

    authentication using next auth for github and google completed

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import { auth , signIn, signOut } from '@/auth'
+
+
+const Navbar =  async() => {
+    const session = await auth();
+  return (
+    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+        <nav className='flex justify-between items-center'>
+            <Link href="/">
+            <Image src="/logo.png" alt = "logo" height={21} width={120}/>
+            </Link>
+
+            <div className='flex items-center gap-5 text-black'>
+                {session && session?.user?(
+                <>
+                <Link href='/startup/create'>
+                    <span>Create</span>
+                </Link>
+                <form action={async()=>{
+                    "use server";
+                    await signOut({redirectTo:"/"});
+                    }}>
+                    <button type='submit'>LogOut</button>
+                </form>
+
+                <Link href={`/user/${session?.id}`}>
+                    <span>{session?.user?.name}</span>
+                </Link>
+                </>    
+            ):(
+                <div className='flex items-center gap-5 text-black'>
+                <form action={async()=>{
+                    "use server";
+                    await signIn('github')
+                    }}>
+                   <button type='submit'>
+                    Login with Github
+                   </button>
+                </form>
+                <form action={async()=>{
+                    "use server";
+                    await signIn('google')
+                    }}>
+                <button type='submit'>Login with Google</button>
+                </form>
+                </div>
+            )}
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
