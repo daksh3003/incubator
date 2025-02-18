fcf77c1f947e64ece9342bbdd82ab68cb0f500d7
daksh3003 <dakshpandey3003@gmail.com>
2025-02-18 23:23:30 +0530

    startup card ui initiated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,9 +6,9 @@ export default async function Home({searchParams}:{searchParams: Promise<{query:
   const query = (await searchParams).query;
   const posts =[
     {
-      _createdAt: 'Yesterday',
+      _createdAt: new Date(),
       views: 55,
-      auhtor:{_id: 1},
+      author:{_id: 1,name:'Daksh'},
       _id: 1,
       description: "This is the description of the startup",
       image:"https://images.unsplash.com/photo-1739433438073-397a07dccdd5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D",
@@ -16,9 +16,9 @@ export default async function Home({searchParams}:{searchParams: Promise<{query:
       title: "ghostbusters"
     },
     {
-      _createdAt: 'Yesterday',
+      _createdAt: new Date(),
       views: 55,
-      auhtor:{_id: 2},
+      author:{_id: 1,name:'Daksh'},
       _id: 2,
       description: "This is the description of the startup",
       image:"https://images.unsplash.com/photo-1739433438073-397a07dccdd5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D",
@@ -26,9 +26,9 @@ export default async function Home({searchParams}:{searchParams: Promise<{query:
       title: "ghostbusters"
     },
     {
-      _createdAt: 'Yesterday',
+      _createdAt: new Date(),
       views: 55,
-      auhtor:{_id: 3},
+      author:{_id: 3,name:'Daksh'},
       _id: 3,
       description: "This is the description of the startup",
       image:"https://images.unsplash.com/photo-1739433438073-397a07dccdd5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D",
@@ -36,9 +36,9 @@ export default async function Home({searchParams}:{searchParams: Promise<{query:
       title: "ghostbusters"
     },
     {
-      _createdAt: 'Yesterday',
+      _createdAt: new Date(),
       views: 55,
-      auhtor:{_id: 4},
+      author:{_id: 1,name:'Daksh'},
       _id: 4,
       description: "This is the description of the startup",
       image:"https://images.unsplash.com/photo-1739433438073-397a07dccdd5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D",
@@ -46,9 +46,9 @@ export default async function Home({searchParams}:{searchParams: Promise<{query:
       title: "ghostbusters"
     },
     {
-      _createdAt: 'Yesterday',
+      _createdAt: new Date(),
       views: 55,
-      auhtor:{_id: 5},
+      author:{_id: 5,name:'Daksh'},
       _id: 5,
       description: "This is the description of the startup",
       image:"https://images.unsplash.com/photo-1739433438073-397a07dccdd5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D",
@@ -71,8 +71,8 @@ export default async function Home({searchParams}:{searchParams: Promise<{query:
       </p>
       <ul className="mt-7 card_grid">
         {posts.length>0 ? (
-          posts.map((StartupCardType,number)=>(
-            <StartupCard/>
+          posts.map((post: StartupCardType)=>(
+            <StartupCard key={post?. _id} post = {post}/>
           ))
         ): (
           <p className="no-results">
