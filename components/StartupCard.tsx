efeafd1645afe2a6ab5ff266067faa4a559956f6
daksh3003 <dakshpandey3003@gmail.com>
2025-02-22 14:20:33 +0530

    sanity query added and fetched

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -43,7 +43,7 @@ const StartupCard = ({post}:{post:StartupCardType}) => {
         </Link>
 
         <div className='flex-between gap-3 mt-5'>
-          <Link href={`/?query=${category.toLowerCase()}`}>
+          <Link href={`/?query=${category? category.toLowerCase():''}`}>
           <p className='text-16-medium'>{category}</p>
           </Link>
           <Button className="startup-card_btn" asChild>
